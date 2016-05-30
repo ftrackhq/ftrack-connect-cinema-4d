b749dbebd877abb19a7f27c46c65a67df8605c92
Lucas Correia <lucas.correia@ftrack.com>
2016-05-30 14:17:41 +0200

    Update install script, remove hardcoded path

--- install_plugin.py
+++ install_plugin.py
@@ -1,27 +1,48 @@
+# :coding: utf-8
+# :copyright: Copyright (c) 2016 ftrack
+
+import os
 import os.path
 import shutil
 
-ROOT_PATH = os.path.dirname(
-    os.path.realpath(__file__)
-)
 
-BUILD_PATH = os.path.join(
-    ROOT_PATH, 'build'
-)
+def install_plugin():
+    '''Install plugin.'''
+    root_path = os.path.dirname(
+        os.path.realpath(__file__)
+    )
 
-STAGING_PATH = os.path.join(
-    BUILD_PATH, 'plugin'
-)
+    build_path = os.path.join(
+        root_path, 'build'
+    )
 
-CINEMA_4D_PLUGIN_DIR = os.path.expanduser(
-    '~/Library/Preferences/MAXON/CINEMA 4D R17_89538A46/plugins/ftrack'
-)
+    staging_path = os.path.join(
+        build_path, 'plugin'
+    )
 
-# Clean staging path
-shutil.rmtree(CINEMA_4D_PLUGIN_DIR, ignore_errors=True)
+    ftrack_connect_cinema_4d_plugin_dir = None
+    try:
+        ftrack_connect_cinema_4d_plugin_dir = os.path.abspath(
+            os.environ['FTRACK_CONNECT_CINEMA_4D_PLUGIN_DIR']
+        )
+    except KeyError:
+        raise ValueError(
+            'Please set the environment variable '
+            '`FTRACK_CONNECT_CINEMA_4D_PLUGIN_DIR` to the installation directory  '
+            'for ftrack-connectc-cinema-4d, e.g.: \n'
+            '`/Users/john/Library/Preferences/MAXON/CINEMA 4D R17_89538A46/plugins/ftrack`.'
+        )
 
-# Copy plugin files
-shutil.copytree(
-    os.path.join(STAGING_PATH, 'ftrack'),
-    CINEMA_4D_PLUGIN_DIR
-)
+    # Clean staging path
+    shutil.rmtree(ftrack_connect_cinema_4d_plugin_dir, ignore_errors=True)
+
+    # Copy plugin files
+    shutil.copytree(
+        os.path.join(staging_path, 'ftrack'),
+        ftrack_connect_cinema_4d_plugin_dir
+    )
+
+
+if __name__ == '__main__':
+    install_plugin()
+    print 'Installed plugin.'
