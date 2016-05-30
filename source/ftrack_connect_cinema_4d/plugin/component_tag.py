f628cea33781c73b1f87a83336b91bd12e15c0e5
Lucas Correia <lucas.correia@ftrack.com>
2016-05-30 16:34:29 +0200

    Remove temp code

--- source/ftrack_connect_cinema_4d/plugin/component_tag.py
+++ source/ftrack_connect_cinema_4d/plugin/component_tag.py
@@ -10,14 +10,10 @@ PLUGIN_ID = 1037465
 class ComponentTag(c4d.plugins.TagData):
     '''Tag plugin to mark the object as an ftrack component.'''
 
-    isObjectActive = False
-
     def Init(self, node):
-        node[c4d.FTRACK_COMPONENT_ID] = '2462518f-6e7e-11e5-9f8a-3c0754289fd3'
         return True
 
     def Execute(self, tag, doc, op, bt, priority, flags):
-        print 'Execute', tag[c4d.FTRACK_COMPONENT_ID]
         return True
 
     def Message(self, node, type, data):
